13b58ff2ca0ccf9cd0510780f144397b28b8c072
isvargasm <ismael.vargas@storecheck.com>
2025-02-20 15:14:45 -0600

    add analitycs vercel use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics/>
         <Navbar />
         <main className="pt-16">{children}</main>
       </body>
